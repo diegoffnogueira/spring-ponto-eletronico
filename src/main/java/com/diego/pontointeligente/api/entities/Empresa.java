dc7aa51fe706db38a467d8211f29d6eb8d18261f
Diego Nogueira <diegof.fnogueira@gmail.com>
2018-02-24 01:36:33 -0300

    Acrescentando as entities, Enums e flyway no projeto

---
+++ src/main/java/com/diego/pontointeligente/api/entities/Empresa.java
@@ -0,0 +1,104 @@
+package com.diego.pontointeligente.api.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "empresa")
+public class Empresa implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long ig;
+
+    @Column(name = "razao_social", nullable = false)
+    private String razaoSocial;
+
+    @Column(name = "cnpj", nullable = false)
+    private String cnpj;
+
+    @Column(name = "data_criacao", nullable = false)
+    private Date dataCriacao;
+
+    @Column(name = "data_atualizacao", nullable = false)
+    private Date dataAtualizacao;
+
+    @OneToMany(mappedBy = "empresa", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Funcionario> funcionarios;
+
+    public Empresa() {
+    }
+
+    public Long getIg() {
+        return ig;
+    }
+
+    public void setIg(Long ig) {
+        this.ig = ig;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(List<Funcionario> funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        dataAtualizacao = new Date();
+    }
+
+    @PrePersist
+    public void prePersist(){
+        final Date dataAtual = new Date();
+        dataCriacao = dataAtual;
+        dataAtualizacao = dataAtual;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa{" +
+                "ig=" + ig +
+                ", razaoSocial='" + razaoSocial + '\'' +
+                ", cnpj='" + cnpj + '\'' +
+                ", dataCriacao=" + dataCriacao +
+                ", dataAtualizacao=" + dataAtualizacao +
+                '}';
+    }
+}
